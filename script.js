e6362362da72cb9242ea74b9347fc02297a463f8
tubi55 <tubi55@nate.com>
2024-09-19 17:52:47 +0900

    함수 파라미터에 전달되는 인수가 없을때 예외처리

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
-//step2 - 반복적인 스타일 정보를 css가 아닌 js에서 동적 적용
-function splitText(elem, tag) {
+//step3 - 세번째 인수로 인터벌값이 있으면 각 반복요소에 누적해서 delay값 적용
+//만약 세번째 인수 없으면 그냥 0으로 초기화
+function splitText(elem, tag = "span", interval = 0) {
+  if (!elem) {
+    console.error("첫 번째 인수값은 텍스를 가져올 DOM선택자를 넣어야 됩니다.");
+    //필수값인 elem요소가 전달되지 않으면 무조건 return으로 함수를 강제 중지해서
+    //불필요한 연산 처리 방지
+    return;
+  }
+
+  console.log("elem이 없을때 불필요한 연산 시작");
+
   const el = document.querySelector(elem);
   const el_text = el.innerText;
-  //아래와 같이 DOM.style.fontSize='0px'은
-  //실제 html태그상에 <h1 style='font-size:0px'></h1>
-  //와 같이 인라인 스타일 형태로 적용
+
   el.style.fontSize = "0px";
   let resultText = "";
+  let counter = 0;
 
   for (let letter of el_text) {
-    // 동적으로 생성되는 문자열 자체적으로 style=''형식으로 스타일 값 연결가능
-    resultText += `<${tag} style='display:inline-block'>${letter}</${tag}>`;
+    resultText += `<${tag} style='display:inline-block; transition-delay:${
+      interval * counter++
+    }s'>${letter}</${tag}>`;
   }
 
   el.innerHTML = resultText;
 }
 
-splitText("h1", "span");
+splitText("h1");
